53d89af70a82db5de79d8a00fe57e6505849ce67
cougar <ridfinity88@gmail.com>
2026-09-14 17:29:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = Object.freeze({
   // นำ URL ที่ลงท้ายด้วย /exec จาก Apps Script Web App มาใส่ตรงนี้
-  GAS_EXEC_URL: 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_EXEC_URL_HERE'
+  GAS_EXEC_URL: 'https://script.google.com/macros/s/AKfycby9ghrjWAVnaYIisQzh88Dg-GNBRD6dfSSsyZTYa47aMWUDCe_7UtV9wV6IOUpFlrJtwA/exec'
 });
